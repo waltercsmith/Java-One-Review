fe014a4c70194c30c82c63454130908f7748030c
Walter Smith <walter.c.smith01@gmail.com>
2021-05-13 22:54:16 -0500

    Feat:Created a main java class and main method that outputs a string

---
+++ src/IntroductionOfWalter.java
@@ -0,0 +1,11 @@
+
+//This class is basically a typical class that would output HelloWorld but with added fields and properties
+public class IntroductionOfWalter {
+
+
+    public static void main(String[] args){
+
+        System.out.println("Hello, Walter! You now an Alumni of Codeup!");
+    }
+
+}
