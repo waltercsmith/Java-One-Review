f39e319fea3617274fb1703c21358f0a0bea4494
Walter Smith <walter.c.smith01@gmail.com>
2021-05-14 19:13:13 -0500

    Feat:Created two properties witin a class. One being a int and another being a string

--- src/IntroductionOfWalter.java
+++ src/IntroductionOfWalter.java
@@ -2,10 +2,16 @@
 //This class is basically a typical class that would output HelloWorld but with added fields and properties
 public class IntroductionOfWalter {
 
+//    Created a property
+    int waltersFavoriteNumber = 308;
+
+//    Created a property that is string
+    String waltersFirstName = "Walter";
+
 
     public static void main(String[] args){
 
-        System.out.println("Hello, Walter! You now an Alumni of Codeup!");
+        System.out.println("Hello, Walter! You are now an Alumni of Codeup!");
     }
 
 }
