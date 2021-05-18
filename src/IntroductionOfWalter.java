f04335f93b72313ac06f71a61b8ff254466383b6
Walter Smith <walter.c.smith01@gmail.com>
2021-05-18 14:05:57 -0500

    Feat:Imported the scanner class. Assigned a new scanner object and specified the system.in for the source of the scanner

--- src/IntroductionOfWalter.java
+++ src/IntroductionOfWalter.java
@@ -1,4 +1,8 @@
 
+//Now I'm going to allow the ability for the user to input into the console
+import java.util.Scanner;
+
+
 //This class is basically a typical class that would output HelloWorld but with added fields and properties
 public class IntroductionOfWalter {
 
@@ -20,6 +24,10 @@ public class IntroductionOfWalter {
 
         String nickName = "WaltWalt";
         System.out.printf("Some of my family members on my mom's side call me %s. I still respond to that nickname", nickName);
+
+//        I have instantiate a new object from the scanner class which was imported to this java class
+        Scanner userInput = new Scanner(System.in);
+
     }
 
 }
